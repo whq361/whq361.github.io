50550ae6ac4197d44ff741d70eb41f94be11f430
whq361 <whq361@qq.com>
2017-06-22 01:23:11 +0800

    delete dzdpapp

--- dzdpapp/app/fetch/new/new.js
+++
@@ -1,6 +0,0 @@
-import { get } from '../get'
-
-export function getAdData() {
-    const result = get('/api/newlist')
-    return result
-}
--- dzdpapp/app/reducers/index.jsx
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import userinfo from './userinfo'
-
-
-// 合并引用的信息数据的规则，并且返出来
-export default combineReducers({
-    userinfo
-})
